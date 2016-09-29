94218f4ef4b515f6efc94213fca9b217f2fd4c6e
virginiachae <chae.virginia@gmail.com>
2016-09-29 11:10:51 -0700

    added alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,17 @@ var cardOne = "queen"
 var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
+
+if (cardOne === cardTwo) {
+	alert('You found a match!')
+} else if (cardThree === cardFour) {
+	alert('You found a match!')
+} else if (cardOne !== cardThree) {
+	alert('Sorry, try again.')
+} else if (cardOne !== cardFour) {
+	alert('Sorry, try again.')
+} else if (cardTwo !== cardThree) {
+	alert('Sorry, try again.')
+} else if (cardTwo !== cardFour) {
+	alert('Sorry, try again.')
+}
